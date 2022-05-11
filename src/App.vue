6aadded1267dce29cbfb99e9ee17c3b203d4b733
yanglongbo <yanglongbo@sohu.com>
2022-05-11 14:06:03 +0800

    feat: Create project catalog file for the first time

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="test-viewport">
+      rem测试转化
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import "~@/assets/css/base.css";
+
+.test-viewport {
+  width: 750px;
+  height: 100px;
+  font-size: 40px;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  line-height: 100px;
+  background: #13b5b1;
 }
 </style>
---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+        <h2>注 册</h2>
+        <form>
+            <div class="form-group">
+                <label class="label" for="name">用户名：</label>
+                <input type="text" class="form-control" id="name" placeholder="请输入用户名" />
+            </div>
+            <div class="form-group">
+                <label class="label" for="password">密码</label>
+                <input type="password" class="form-control" id="password" placeholder="请输入密码" />
+            </div>
+            <div class="form-group">
+                <label class="label" for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="请输入Email" />
+            </div>
+            <div class="form-group">
+                <input class="form-checkbox" id="checkbox" type="checkbox" value="value" />
+                <label class="labelb" for="checkbox">勾选登录</label>
+            </div>
+            <button type="button" class="btn">登录</button>
+        </form>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+body {
+    position: relative;
+    font-size: 13px;
+    color: #505050;
+    background: #fff;
+    height:100%;
+}
+.container {
+    width: 200px;
+    margin: 0 auto;
+}
+h2 {
+    text-align: center;
+}
+.form-control {
+    width: 200px;
+    height: 23px;
+}
+.form-group,
+.btn {
+    margin-top: 10px;
+}
+.form-group>.label {
+    display: block;
+}
+.form-group>.form-control {
+    border: 1px solid #bfc0c5;
+}
+.form-group>.form-control:hover {
+    border: 1px solid #474d5b;
+    color: #383e4b;
+}
+</style>
